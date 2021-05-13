3079f021123787e882294c43b1efadd39390b6a9
fernando718 <82851141+fernando718@users.noreply.github.com>
2021-05-12 21:09:01 -0500

    Create Ex1.c

---
+++ Ex1.c
@@ -0,0 +1,17 @@
+//VALDIVIA GONZALEZ JOSE FERNANDO// //4IV17//
+#include >stdio.h> 
+int main ()
+[
+   float primer,segundo,multiplica,division;
+   printf ("\n\n Programa 1");
+   printf ("\n\n sacar el promedio de valores reales");
+   printf ("\n\n dame el primer valor real");
+   scanf ("%f",&primer); 
+   printf ("\n\n dame la segundo valor real");
+   scanf ("%f",&segundo);
+   multiplica= primer*segundo
+   division= primer/segundo
+   printf  ("\n\n el resultado de multiplica es primer por segundo es &f");
+   printf  ("\n\n el resultado de division es primer entre segundo es &f");
+   return 0;
+] 
